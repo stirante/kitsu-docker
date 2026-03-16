1ac995a796fb11e7f5d6fca93f5ea3b18ebd63ce
stirante <brzozowski.s.piotr@gmail.com>
2026-03-16 13:18:46 +0100

    Add local versions of docker files

--- docker/start_zou.sh
+++ docker/start_zou.sh
@@ -4,5 +4,12 @@
 . /usr/share/postgresql-common/init.d-functions
 create_socket_directory
 
+service postgresql start
+
+. /opt/zou/env/bin/activate
+zou upgrade-db
+
+service postgresql stop
+
 echo Running Zou...
 supervisord -c /etc/supervisord.conf
